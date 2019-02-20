acfc79c5bcbfbeb650a98c5599deefc7dc7dc568
Kevlar <kevin.j.cross@gmail.com>
2019-02-20 14:47:26 +0000

    added merging capability

--- __tests__/propertyVolumeTests.ts
+++ __tests__/propertyVolumeTests.ts
@@ -1,4 +1,6 @@
 import * as properties from '../src'
+
+process.env['NODE_CONFIG_DIR'] = __dirname + '/config/'
 import * as config from 'config'
 
 describe('Read properties from files on a mount or folder.', () => {
@@ -17,5 +19,13 @@ describe('Read properties from files on a mount or folder.', () => {
     expect(config.get('keyVault.vaultOne.secret_Three')).toBe('vaultOne.secret_Three')
     expect(config.get('keyVault.vaultTwo.secret-one')).toBe('vaultTwo.secret-one')
     expect(config.get('keyVault.vaultTwo.secretTwo')).toBe('vaultTwo.secretTwo')
+    expect(config.get('keyVault.vaultX.secret-one')).toBe('x')
   })
+
+  test('should have defaults (existing properties if not overridden', () => {
+    expect(config.get('keyVault.vaultOne.secret-default')).toBe('default')
+    expect(config.get('keyVault.vaultTwo.secret-default')).toBe('default2')
+    expect(config.get('keyVault.someOther_vault.secret-default')).toBe('someother')
+  })
+
 })
--- src/Properties.ts
+++ src/Properties.ts
@@ -5,8 +5,13 @@ const log = Logger.getLogger('applicationRunner')
 
 export function addTo (config: any, mountPoint: fs.PathLike = '/mnt/secrets/', propertiesPrefix: string = 'keyVault') {
   log.info(`Reading properties from volume: '${mountPoint}'`)
-  config[propertiesPrefix] = fs.readdirSync(mountPoint, null)
-    .reduce((obj, dir) => addDir(dir, obj, mountPoint), {})
+  const vaults = config[propertiesPrefix]
+  const properties = readVaults(mountPoint)
+  if (vaults == null) {
+    config[propertiesPrefix] = properties
+  } else {
+    mergeVaults(vaults, properties)
+  }
 }
 
 function addDir (dir: string, obj: any, mountPoint: fs.PathLike): any {
@@ -25,4 +30,14 @@ function readFile (mountPoint: fs.PathLike, dir: string, file: string): string {
 
 function readDirectories (mountPoint: fs.PathLike, dir: string): string[] {
   return fs.readdirSync(mountPoint + '/' + dir)
+}
+
+function readVaults (mountPoint: fs.PathLike) {
+  return fs.readdirSync(mountPoint, null).reduce((obj, dir) => addDir(dir, obj, mountPoint), {})
+}
+
+function mergeVaults (vaults: any, properties: any) {
+  for (const key in properties) {
+    vaults[key] = Object.assign(vaults[key] || {}, properties[key])
+  }
 }
