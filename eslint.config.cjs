774d74afcf27d298dca47ccd24bde481cfe57750
Michael1142 <Michael1142@users.noreply.github.com>
2026-01-06 17:04:10 +0000

    Jan patch 1 (#487)
    * Update renovate config
    
    * Update dependencies
    
    * Update dependencies
    
    * Update to eslint 9
    
    * Update jest to 30
    
    * Remove comments

---
+++ eslint.config.cjs
@@ -0,0 +1,88 @@
+const js = require("@eslint/js");
+const globals = require("globals");
+const { FlatCompat } = require("@eslint/eslintrc");
+
+const tsParser = require("@typescript-eslint/parser");
+const tsPlugin = require("@typescript-eslint/eslint-plugin");
+const importPlugin = require("eslint-plugin-import");
+const jestPlugin = require("eslint-plugin-jest");
+const prettierPlugin = require("eslint-plugin-prettier");
+
+const compat = new FlatCompat({ baseDirectory: __dirname });
+
+module.exports = [
+  js.configs.recommended,
+
+  ...compat.extends(
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "plugin:jest/recommended",
+    "plugin:prettier/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended"
+  ),
+
+  {
+    files: ["**/*.{js,cjs,mjs,ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2018,
+      sourceType: "module",
+      parser: tsParser,
+      parserOptions: {
+        project: "./tsconfig.json",
+        tsconfigRootDir: __dirname,
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2015,
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+      import: importPlugin,
+      jest: jestPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      "@typescript-eslint/array-type": "error",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-shadow": "error",
+      "@typescript-eslint/no-unused-vars": ["error", { ignoreRestSiblings: true }],
+      "@typescript-eslint/no-var-requires": "off",
+      curly: "error",
+      eqeqeq: "error",
+      "import/no-duplicates": "error",
+      "import/no-named-as-default": "error",
+      "import/order": "off",
+      "jest/prefer-to-have-length": "error",
+      "linebreak-style": ["error", "unix"],
+      "no-console": "warn",
+      "no-prototype-builtins": "off",
+      "no-return-await": "error",
+      "no-unneeded-ternary": ["error", { defaultAssignment: false }],
+      "object-curly-spacing": ["error", "always"],
+      "object-shorthand": ["error", "properties"],
+      quotes: ["error", "single", { allowTemplateLiterals: false, avoidEscape: true }],
+      semi: ["error", "always"],
+      "sort-imports": [
+        "error",
+        {
+          allowSeparatedGroups: false,
+          ignoreCase: false,
+          ignoreDeclarationSort: true,
+          ignoreMemberSort: false,
+        },
+      ],
+    },
+  },
+
+  {
+    files: ["**/*.{test,spec}.{js,ts,tsx}", "**/__tests__/**/*.{js,ts,tsx}"],
+    languageOptions: { globals: { ...globals.jest } },
+  },
+];
